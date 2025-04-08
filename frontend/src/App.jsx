9937f31a3fc162687eca577f974973c88943044c
Emery <mremmanueledem@gmail.com>
2025-04-09 00:02:43 +0100

    added comment for the checkauth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,30 +5,42 @@ import Dashboard from "./pages/DashboardPage";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ToastBar, Toaster } from "react-hot-toast";
 import { useUserStore } from "./store/useUserStore";
-import { use, useEffect } from "react";
+import { useEffect } from "react";
 
 function App() {
-  const { user, checkAuth } = useUserStore();
+  const { user, checkAuth, checkingAuth } = useUserStore();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={!user ? <LoginPage /> : <Navigate to="/dashboard" />}
-          />
+  if (checkingAuth) {
+    return <div>Loading...</div>;
+  }
 
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-        <Toaster />
-      </BrowserRouter>
-    </div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Home ("/") and login both redirect if user is already logged in */}
+        <Route
+          path="/"
+          element={user ? <Navigate to="/dashboard" /> : <LoginPage />}
+        />
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/dashboard" /> : <LoginPage />}
+        />
+        <Route
+          path="/signup"
+          element={user ? <Navigate to="/dashboard" /> : <SignupPage />}
+        />
+        <Route
+          path="/dashboard"
+          element={user ? <Dashboard /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      <Toaster />
+    </BrowserRouter>
   );
 }
 
