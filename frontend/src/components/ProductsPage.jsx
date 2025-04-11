f9ec4cc71a683ea59d789c1082abab21a7cefbee
Emery <mremmanueledem@gmail.com>
2025-04-11 16:20:53 +0100

    feat: added small design changes to the products page.

--- frontend/src/components/ProductsPage.jsx
+++ frontend/src/components/ProductsPage.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { Plus, Loader2 } from "lucide-react";
+import { Plus, Loader2, Upload } from "lucide-react";
 import { useProductStore } from "../store/useProductStore";
 import toast from "react-hot-toast";
 import { motion } from "framer-motion";
+import ProductsList from "./ProductsList";
 
 const categories = ["jeans", "shirts", "shoes", "accessories"];
 export default function CreateProductPage() {
@@ -15,10 +16,13 @@ export default function CreateProductPage() {
     image: "",
   });
 
-  const { createProduct, loading } = useProductStore();
+  const { createProduct, fetchAllProducts } = useProductStore();
+  const [isCreating, setIsCreating] = useState(false);
+  const [uploadedFileName, setUpLoadedFileName] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsCreating(true);
 
     try {
       await createProduct(newProduct);
@@ -30,8 +34,11 @@ export default function CreateProductPage() {
         image: "",
       });
       toast.success("Product created successfully!");
+      await fetchAllProducts(); // Fetch all products after creating a new one
     } catch (error) {
       console.error("Error creating product:", error);
+    } finally {
+      setIsCreating(false);
     }
   };
 
@@ -54,6 +61,7 @@ export default function CreateProductPage() {
         }));
       };
       reader.readAsDataURL(file);
+      setUpLoadedFileName(file.name);
     }
   };
 
@@ -120,15 +128,29 @@ export default function CreateProductPage() {
 
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-1">
-                Image URL
+                Image
               </label>
               <input
                 type="file"
-                name="image"
+                id="image"
+                className="sr-only"
                 accept="image/*"
                 onChange={handleImageChange}
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
+              <label
+                htmlFor="image"
+                className="w-full flex items-center px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer"
+              >
+                <Upload className="h-5 w-5 inline-block mr-2 text-gray-500" />
+                <span className="text-sm text-gray-700">
+                  {!uploadedFileName ? "Upload Image" : uploadedFileName}
+                </span>
+              </label>
+              {newProduct.image && (
+                <span className="ml-1 text-sm text-green-400">
+                  Image uploaded ✅
+                </span>
+              )}
             </div>
 
             <div className="md:col-span-2">
@@ -150,15 +172,16 @@ export default function CreateProductPage() {
             <button
               type="submit"
               className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 flex items-center"
-              disabled={loading}
+              disabled={isCreating}
             >
               <Plus size={18} className="mr-2" />
-              {loading && <Loader2 className="animate-spin" size={18} />}
-              {loading ? "Uploading..." : "Create Product"}
+              {isCreating && <Loader2 className="animate-spin" size={18} />}
+              {isCreating ? "Uploading..." : "Create Product"}
             </button>
           </div>
         </form>
       </div>
+      <ProductsList />
     </motion.div>
   );
 }
